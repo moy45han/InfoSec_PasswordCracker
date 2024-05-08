c88bbece413c2f7ff9ed7b2f79db0bb0d588a093
Ernest Leong <moy45han@hotmail.com>
2024-05-08 22:12:41 +0000

    Working version

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 import password_cracker
 from unittest import main
 
-cracked_password1 = password_cracker.crack_sha1_hash(
-    "fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2")
+cracked_password1 = password_cracker.crack_sha1_hash("fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2")
+#cracked_password1 = password_cracker.crack_sha1_hash("b305921a3723cd5d70a375cd21a61e60aabb84ec")            #sammy123
+#cracked_password1 = password_cracker.crack_sha1_hash("c7ab388a5ebefbf4d550652f1eb4d833e5316e3e")            #abacab
+#cracked_password1 = password_cracker.crack_sha1_hash("5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8")            #password
 print(cracked_password1)
 
-cracked_password2 = password_cracker.crack_sha1_hash(
-    "dcc466796201f7232b22a03781110a8871fd038c", True)
+cracked_password2 = password_cracker.crack_sha1_hash("dcc466796201f7232b22a03781110a8871fd038c", True)
+#cracked_password2 = password_cracker.crack_sha1_hash("53d8b3dc9d39f0184144674e310185e41a87ffd5", True)     #superman
+#cracked_password2 = password_cracker.crack_sha1_hash("da5a4e8cf89539e66097acd2f8af128acae2f8ae", True)     #q1w2e3r4t5
+#cracked_password2 = password_cracker.crack_sha1_hash("ea3f62d498e3b98557f9f9cd0d905028b3b019e1", True)     #bubbles1
 print(cracked_password2)
 
 # Run unit tests automatically
--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,37 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    bFound = False
+    returnVal = ""
+
+    with open('top-10000-passwords.txt', 'r') as f: 
+        for pwd_line in f.readlines():
+            h=hashlib.sha1()    
+            
+            #strip out new line characters
+            pwd = pwd_line.strip()
+
+            if (use_salts == True):
+                with open('known-salts.txt', 'r') as fsalts: 
+                    for salt_line in fsalts.readlines():
+                         #strip out new line characters
+                        salt = salt_line.strip()
+
+                        #prepend salt to password
+                        pwd_salt_prepend = salt + pwd 
+                        #append salt to password
+                        pwd_salt_append = pwd + salt
+                        
+                        hashed_salt_prepend = hashlib.sha1(pwd_salt_prepend.encode()).hexdigest()
+                        hashed_salt_append = hashlib.sha1(pwd_salt_append.encode()).hexdigest()
+
+                        if ((hash==hashed_salt_prepend) or (hash==hashed_salt_append)):
+                            return pwd
+            else:  
+                hashed_pwd = hashlib.sha1(pwd.encode()).hexdigest()
+
+                if (hash==hashed_pwd):
+                    return pwd
+        
+    #return 
+    return "PASSWORD NOT IN DATABASE"
